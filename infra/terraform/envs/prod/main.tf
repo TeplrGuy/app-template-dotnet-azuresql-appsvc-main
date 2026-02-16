51ca673f476b3b2880e7b2c423d410a6a2ace869
Gibs <gappiah@microsoft.com>
2026-02-15 20:58:22 -0500

    fix(tf): enable OIDC and Azure AD auth for Terraform backend state storage

--- infra/terraform/envs/prod/main.tf
+++ infra/terraform/envs/prod/main.tf
@@ -9,7 +9,9 @@ terraform {
   }
 
   backend "azurerm" {
-    # Configured via -backend-config flags in CI:
+    use_oidc         = true
+    use_azuread_auth = true
+    # Remaining config via -backend-config flags in CI:
     #   -backend-config="resource_group_name=..."
     #   -backend-config="storage_account_name=..."
     #   -backend-config="container_name=tfstate"
