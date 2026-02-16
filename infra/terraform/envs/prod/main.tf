cca6d147e2cced8f5f36d255b4bfe0f758148c6b
Gibs <gappiah@microsoft.com>
2026-02-15 20:56:48 -0500

    fix(cd): add Docker build/push to CD pipelines, enable OIDC in Terraform provider
    - CD-staging/prod: build and push Docker images before deploying
    - Terraform provider: add use_oidc=true and storage_use_azuread=true
    - Health checks: increase wait time and retry count for container startup

--- infra/terraform/envs/prod/main.tf
+++ infra/terraform/envs/prod/main.tf
@@ -20,9 +20,11 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
+  subscription_id            = var.subscription_id
+  tenant_id                  = var.tenant_id
+  client_id                  = var.client_id
+  use_oidc                   = true
+  storage_use_azuread        = true
 }
 
 locals {
