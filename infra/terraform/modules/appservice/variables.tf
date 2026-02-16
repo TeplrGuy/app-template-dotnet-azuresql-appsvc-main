36d9f0d4dfae05fae40335f55261f2c0c513d34e
Gibs <gappiah@microsoft.com>
2026-02-15 21:28:08 -0500

    fix(tf): revert App Service Plan SKU to B1, switching to swedencentral region

--- infra/terraform/modules/appservice/variables.tf
+++ infra/terraform/modules/appservice/variables.tf
@@ -26,7 +26,7 @@ variable "frontend_app_name" {
 variable "sku_name" {
   description = "App Service Plan SKU name (e.g., B1, S1, P1v3)."
   type        = string
-  default     = "P0v3"
+  default     = "B1"
 }
 
 variable "always_on" {
